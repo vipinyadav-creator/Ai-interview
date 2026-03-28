25485b14ec62bb44d896464a8e7121f7124ea85b
Vipin Yadav <vipinyadav4926@gmail.com>
2026-03-28 13:03:44 +0530

    Fix vite config and update build with base path

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -13,6 +13,7 @@ process.env.STORAGE_GATEWAY_URL =
   process.env.STORAGE_GATEWAY_URL || "https://blob.caffeine.ai";
 
 export default defineConfig({
+  base: "/Ai-interview/",
   logLevel: "error",
   build: {
     emptyOutDir: true,
